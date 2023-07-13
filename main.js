c4563df2042b0d0c85fba4e0299098cd648aacec
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-07-13 19:50:23 +0200

    css and js for class board

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
 let board = document.querySelector(".board");
 board.computedStyleMap.gridTemplateColumns = "repeat(16, 1fr)"
+
+for(let i = 0; i<256; i++){
+    let square = document.createElement('div')
+    square.style.backgroundColor = "blue"
+}
