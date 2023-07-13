26c27074409114588b2b08b678950cd3ab456dd5
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-07-13 21:16:39 +0200

    populate Board function

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-function populateBoard{
+function populateBoard(size){
 let board = document.querySelector(".board");
-board.style.gridTemplateColumns = "repeat(16, 1fr)";
-board.style.gridTemplateRows = "repeat(16, 1fr)";
+board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
 for(let i = 0; i<256; i++){
     let square = document.createElement('div')
     square.style.backgroundColor = "blue";
     board.insertAdjacentElement("beforeend",square)
 }
+}
+populateBoard(16);
+
+function changesSize(input){
+    populateBoard()
 }
