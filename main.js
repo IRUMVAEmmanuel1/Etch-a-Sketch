175d17ba3accf8ec286ce28088327229877338da
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-07-13 20:55:57 +0200

    css and js for class board

--- main.js
+++ main.js
@@ -4,5 +4,6 @@ board.style.gridTemplateRows = "repeat(16, 1fr)";
 
 for(let i = 0; i<256; i++){
     let square = document.createElement('div')
-    square.style.backgroundColor = "blue"
+    square.style.backgroundColor = "blue";
+    board.insertAdjacentElement("beforeend",square)
 }
